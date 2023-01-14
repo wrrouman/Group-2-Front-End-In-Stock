7652e8d26ce552fdcd26b9f992333f86d4198f84
wrrouman <106677200+wrrouman@users.noreply.github.com>
2023-01-14 09:47:01 -0500

    Initial add Warhouse component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-
 import "./App.css";
-import Footer from './components/footer/Footer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//import components
+import Add_Warehouse from "./components/Add_Warehouse/Add_Warhouse";
+import Footer from "./components/footer/Footer";
 function App() {
   return (
-    <div className="App">
-  <Footer/>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Add" element={<Add_Warehouse />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Add_Warehouse/Add_Warhouse.js
@@ -0,0 +1,65 @@
+import './Add_Warehouse.scss';
+
+//import images
+import Arrow_Back from "../../assets/Icons/arrow_back-24px.svg"; 
+
+function Add_Warehouse() {
+    return (
+        <>
+            <div className="warehouse_add_popup">
+                <div className="warehouse_add_header">
+                    <img src={Arrow_Back}/>
+                    <p>Edit Warehouse</p>
+                </div>
+
+                <form className="warehouse_add_form">
+                    <div className="warehouse_add_block">
+                        <div className="warehouse_add_block1">
+                            <p>Warehouse Details</p>
+
+                            <label >Warehouse Name</label>
+                            <input placeholder="Warehouse Name" />
+
+                            <label>Street Address</label>
+                            <input placeholder="Street Address" />
+
+                            <label>City</label>
+                            <input placeholder="City"/>
+
+                            <label>Country</label>
+                            <input placeholder="Country" />
+                        </div>
+
+                        <div className="warehouse_add_block2">
+                            <p>Contact Details</p>
+
+                            <label>Contact Name</label>
+                            <input placeholder="Contact Name" />
+
+                            <label>Position</label>
+                            <input placeholder="Position" />
+
+                            <label>Phone Number</label>
+                            <input placeholder="Phone Number" />
+
+                            <label>Email</label>
+                            <input placeholder="Email" />
+                        </div>
+
+                    </div>
+                    
+
+                    <div className="warehouse_addbutton_block">
+                        <button>Cancel</button>
+                        <button>+Add Warehouse</button>
+                    </div>
+
+                </form>
+               
+            </div>
+            
+        </>
+    );
+}
+
+export default Add_Warehouse;
