b8e731c91d77f612e32fa5664752d302b9dd6a80
Howard Yen <hwrdpvt@gmail.com>
2023-01-14 18:02:46 -0500

    Updated 'Edit Warehouse' mobile/tablet/desktop CSS style

--- src/components/Warehouse_Edit/Warehouse_Edit.js
+++ src/components/Warehouse_Edit/Warehouse_Edit.js
@@ -12,54 +12,54 @@ function Warehouse_Edit() {
             <p>{ warehouseID }</p>
             <div className="Warehouse-Edit__page">
                 <div className="Warehouse-Edit__header">
-                    <NavLink to={"/warehouse_details/123"}>
+                    <NavLink to={"/warehouse_details/123"} className="Warehouse-Edit__header--arrowbackicon-link">
                     {/* <NavLink to={`/warehouse_details/${warehouseID}`}> */}
-                        <img src={Arrow_Back}/>
+                        <img src={Arrow_Back} className="Warehouse-Edit__header--arrowbackicon"/>
                     </NavLink>
 
-                    <p>Edit Warehouse</p>
+                    <p className="Warehouse-Edit__header--title">Edit Warehouse</p>
                 </div>
 
                 <form className="Warehouse-Edit__form">
                     <div className="Warehouse-Edit__block">
                         <div className="Warehouse-Edit__formblock--warehouse-details">
-                            <p>Warehouse Details</p>
+                            <p className="Warehouse-Edit__formblock--title">Warehouse Details</p>
 
-                            <label >Warehouse Name</label>
-                            <input placeholder="Washington" />
+                            <label className="Warehouse-Edit__warehouse-details--title">Warehouse Name</label>
+                            <input placeholder="Washington" className="Warehouse-Edit__warehouse-details--input"/>
 
-                            <label>Street Address</label>
-                            <input placeholder="33 Pearl Street SW" />
+                            <label className="Warehouse-Edit__warehouse-details--title">Street Address</label>
+                            <input placeholder="33 Pearl Street SW" className="Warehouse-Edit__warehouse-details--input"/>
 
-                            <label>City</label>
-                            <input placeholder="Washington"/>
+                            <label className="Warehouse-Edit__warehouse-details--title">City</label>
+                            <input placeholder="Washington" className="Warehouse-Edit__warehouse-details--input"/>
 
-                            <label>Country</label>
-                            <input placeholder="USA" />
+                            <label className="Warehouse-Edit__warehouse-details--title">Country</label>
+                            <input placeholder="USA" className="Warehouse-Edit__warehouse-details--input"/>
                         </div>
 
                         <div className="Warehouse-Edit__formblock--contact-details">
-                            <p>Contact Details</p>
+                            <p className="Warehouse-Edit__formblock--title">Contact Details</p>
 
-                            <label>Contact Name</label>
-                            <input placeholder="Graeme Lyon" />
+                            <label className="Warehouse-Edit__contact-details--title">Contact Name</label>
+                            <input placeholder="Graeme Lyon" className="Warehouse-Edit__contact-details--input"/>
 
-                            <label>Position</label>
-                            <input placeholder="Warehouse Manager" />
+                            <label className="Warehouse-Edit__contact-details--title">Position</label>
+                            <input placeholder="Warehouse Manager" className="Warehouse-Edit__contact-details--input"/>
 
-                            <label>Phone Number</label>
-                            <input placeholder="+1 (647) 504-0911" />
+                            <label className="Warehouse-Edit__contact-details--title">Phone Number</label>
+                            <input placeholder="+1 (647) 504-0911" className="Warehouse-Edit__contact-details--input"/>
 
-                            <label>Email</label>
-                            <input placeholder="glyon@instock.com" />
+                            <label className="Warehouse-Edit__contact-details--title">Email</label>
+                            <input placeholder="glyon@instock.com" className="Warehouse-Edit__contact-details--input"/>
                         </div>
 
                     </div>
                     
 
                     <div className="Warehouse-Edit__buttonblock">
-                        <button>Cancel</button>
-                        <button>Save</button>
+                        <button className="Warehouse-Edit__cancelbutton">Cancel</button>
+                        <button className="Warehouse-Edit__savebutton">Save</button>
                     </div>
 
                 </form>
