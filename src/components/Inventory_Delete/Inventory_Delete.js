1ef482a30f158d897dc7afcc947745ae8e15afa4
Howard Yen <hwrdpvt@gmail.com>
2023-01-14 12:54:01 -0500

    Second Draft - button actions (with fake warehouseID)

--- src/components/Inventory_Delete/Inventory_Delete.js
+++ src/components/Inventory_Delete/Inventory_Delete.js
@@ -1,11 +1,27 @@
 import "./Inventory_Delete.scss";
+import { useState, useEffect} from 'react';
 import { NavLink, useParams } from 'react-router-dom';
 
 //import images
 import Close from "../../assets/Icons/close-24px.svg";
 
+//import JSON files
+import Inventory_Json from "../../server/data/inventories.json";
+import Warehouse_Json from "../../server/data/warehouses.json";
+
 function Inventory_Delete() {
     let { inventoryID } = useParams();
+
+    //useState variable
+    const [InventoryJson, setInventoryJson] = useState(Inventory_Json);
+    const [CurrentInventoryJson, setCurrentInventoryJson] = useState([]);
+
+    useEffect(() => {
+        let current_inventory = InventoryJson.filter(x => x.id === inventoryID);
+        setCurrentInventoryJson(current_inventory);
+    }, [inventoryID]);
+
+    console.log(CurrentInventoryJson[0]?.itemName);
 
     return(
         <>
@@ -16,7 +32,7 @@ function Inventory_Delete() {
                     <img src={Close}/>
                 </NavLink>
 
-                <p>Delete (Television) inventory item?</p>
+                <p>Delete ({CurrentInventoryJson[0]?.itemName}) inventory item?</p>
                 <p>Please confirm that you'd like to delete Television from the inventory list. You won't be able to undo this action</p>
                 <button>Cancel</button>
                 <button>Delete</button>
