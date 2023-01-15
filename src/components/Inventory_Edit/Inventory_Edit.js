06e11cc6b1bd7f872f8208e4aca3f93595642b83
Howard Yen <hwrdpvt@gmail.com>
2023-01-14 20:55:09 -0500

    Updated 'Edit Inventory Item' mobile/tablet/desktop CSS style

--- src/components/Inventory_Edit/Inventory_Edit.js
+++ src/components/Inventory_Edit/Inventory_Edit.js
@@ -12,28 +12,28 @@ function Inventory_Edit() {
             <p>{ inventoryID }</p>
             <div className="Inventory-Edit__page">
                 <div className="Inventory-Edit__header">
-                    <NavLink to={`/warehouse_details/123`}>
+                    <NavLink to={`/warehouse_details/123`} className="Inventory-Edit__header--arrowbackicon-link">
                     {/* <NavLink to={`/warehouse_details/${warehouseID}`}> */}
-                        <img src={Arrow_back}/>
+                        <img src={Arrow_back} className="Inventory-Edit__header--arrowbackicon"/>
                     </NavLink>
 
-                    <p>Edit Inventory</p>
+                    <p className="Inventory-Edit__header--title">Edit Inventory Item</p>
                 </div>
 
-                <form>
+                <form className="Inventory-Edit__form">
                     <div className="Inventory-Edit__block">
                         <div className="Inventory-Edit__block--item-details">
-                            <p>Item Details</p>
+                            <p className="Inventory-Edit__formblock--title">Item Details</p>
 
-                            <label>Item Name</label>
-                            <input placeholder="Television"/>
+                            <label className="Inventory-Edit__item-details--title">Item Name</label>
+                            <input placeholder="Television" className="Inventory-Edit__item-details--input"/>
 
-                            <label>Description</label>
-                            <textarea placeholder='This 50", 4K LED TV provides a crystal-clear picture and vivid colors.'/>
+                            <label className="Inventory-Edit__item-details--title">Description</label>
+                            <textarea placeholder='This 50", 4K LED TV provides a crystal-clear picture and vivid colors.' className="Inventory-Edit__item-details--textarea"/>
 
-                            <label>Category</label>
-                            <select>
-                                <option value="electronics">Electronics</option>
+                            <label className="Inventory-Edit__item-details--title">Category</label>
+                            <select className="Inventory-Edit__item-details--select">
+                                <option value="electronics" selected>Electronics</option>
                                 <option value="gear">Gear</option>
                                 <option value="apparel">Apparel</option>
                                 <option value="accessories">Accessories</option>
@@ -42,26 +42,26 @@ function Inventory_Edit() {
                         </div>
                         
                         <div className="Inventory-Edit__block--item-availability">
-                            <p>Item Availability</p>
+                            <p className="Inventory-Edit__formblock--title">Item Availability</p>
 
-                            <label>Status</label>
+                            <label className="Inventory-Edit__item-availability--title">Status</label>
                             <div className="Inventory-Edit__block--status">
-                                <div>
-                                    <input type="radio" id="instock" name="stockstatus" value="In stock"></input>
-                                    <label for="instock">In stock</label>
+                                <div className="Inventory-Edit__block--status-block">
+                                    <input type="radio" id="instock" name="stockstatus" value="In stock" className="Inventory-Edit__block--status-input"></input>
+                                    <label for="instock" className="Inventory-Edit__block--status-label">In stock</label>
                                 </div>
 
-                                <div>
-                                    <input type="radio" id="outofstocks" name="stockstatus" value="Out of stock"></input>
-                                    <label for="outofstock">Out of stock</label>
+                                <div className="Inventory-Edit__block--status-block">
+                                    <input type="radio" id="outofstocks" name="stockstatus" value="Out of stock" className="Inventory-Edit__block--status-input"></input>
+                                    <label for="outofstock" className="Inventory-Edit__block--status-label">Out of stock</label>
                                 </div>
                             </div>
 
 
 
-                            <label>Warehouse</label>
-                            <select>
-                                <option value="manhattan">Manhattan</option>
+                            <label className="Inventory-Edit__item-availability--title">Warehouse</label>
+                            <select className="Inventory-Edit__item-availability--select">
+                                <option value="manhattan" selected>Manhattan</option>
                                 <option value="washington">Washington</option>
                                 <option value="jersey">Jersey</option>
                                 <option value="san_fran">San Fran</option>
@@ -74,8 +74,8 @@ function Inventory_Edit() {
                     </div>
                     
                     <div className="Inventory-Edit__block--editbutton">
-                        <button>Cancel</button>
-                        <button>Save</button>
+                        <button className="Inventory-Edit__editbutton--cancel">Cancel</button>
+                        <button className="Inventory-Edit__editbutton--save">Save</button>
                     </div>
 
                 </form>
